bb72adf8dab38aadfd76a038f8dcd1870331e857
Ivan Zavadsky <i.zavadsky@itransition.com>
2020-11-27 15:00:32 +0300

    init

---
+++ Namshi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Namshi
+//
+//  Created by MacBook on 11/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
